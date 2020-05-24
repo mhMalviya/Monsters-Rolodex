bfdd134b10ea6ab8265996befb289c6e4c19101a
Mayank Malviya <mhmalviya94@google.com>
2020-05-24 17:49:03 +0530

    Updates

---
+++ precache-manifest.a65ab3cf6348919d71fb59cc0d5f28aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "314dd948f40dc4d63882d42d42cc3d4e",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "5480b76005b96163492a",
+    "url": "/Monsters-Rolodex/static/css/main.80e76a9c.chunk.css"
+  },
+  {
+    "revision": "3d6a108183c948b6fb2e",
+    "url": "/Monsters-Rolodex/static/js/2.a5409b05.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/Monsters-Rolodex/static/js/2.a5409b05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5480b76005b96163492a",
+    "url": "/Monsters-Rolodex/static/js/main.d42dfcda.chunk.js"
+  },
+  {
+    "revision": "5bb244a38e825d0ab28e",
+    "url": "/Monsters-Rolodex/static/js/runtime-main.129d40ef.js"
+  }
+]);
